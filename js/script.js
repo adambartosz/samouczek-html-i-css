05c5d05183ee3530ada613c1ae11bd0a3d89fd05
Adam Bartosz <adambartosz1116@gmail.com>
2026-05-31 16:18:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@
         layout.appendChild(nav);
 
         const header = document.createElement("header");
-        header.textContent = document.title;
+        header.innerHTML = `<h1>document.title</h1>`;
         layout.prepend(header);
 
         const buttonNav = document.createElement("button");
